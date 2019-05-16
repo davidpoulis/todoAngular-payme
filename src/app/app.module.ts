43ee1b1fafeab8d4f42950dc6db2ec6d763196a6
David Poules <david_fci@outlook.com>
2019-05-16 02:43:01 +0200

    basic structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { TasksComponent } from './tasks/tasks.component';
+import { TaskDetailComponent } from './task-detail/task-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HeaderComponent,
+    HomeComponent,
+    AboutComponent,
+    TasksComponent,
+    TaskDetailComponent
   ],
   imports: [
     BrowserModule,
